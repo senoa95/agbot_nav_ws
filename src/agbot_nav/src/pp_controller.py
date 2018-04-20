64fc230917f2be4a95678159ffc6f23e6317d18c
senoa95 <sena@vt.edu>
2018-04-19 20:21:47 -0400

    added AckermannVehicle Class

--- src/agbot_nav/src/pp_controller.py
+++ src/agbot_nav/src/pp_controller.py
@@ -6,7 +6,6 @@ from geometry_msgs.msg import Point32,Pose
 import transforms3d as tf
 import numpy as np
 
-
 pi = 3.141592653589793238
 
 class Point:
@@ -15,3 +14,11 @@ class Point:
         self.x = inputX
         self.y = inputY
         self.heading = inputHeading
+class AckermannVehicle:
+    #default ackermann characteristics
+
+    def __init__(self,inputLength = 1,inputMaximumSteeringAngle = 1,inputMaximumVelocity = 48*pi/180):
+        self.length = inputLength
+        self.maximumSteeringAngle = inputMaximumSteeringAngle
+        self.maximumVelocity = inputMaximumVelocity
+        self.minTurningRadius = self.length / math.tan(self.maximumSteeringAngle)
