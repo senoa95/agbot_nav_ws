d299f3fd7b5e2f5df6d53c71b03a97f277b1c841
vTechSavyy <psavio5@vt.edu>
2018-04-20 16:34:11 -0400

    Fixed steering command according to Pure pursuit paper

--- src/agbot_nav/src/pp_controller.py
+++ src/agbot_nav/src/pp_controller.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import rospy
 import math
 from geometry_msgs.msg import Point32,Pose
@@ -22,7 +21,6 @@ currentPoint = Point()
 
 reachedGoal = False
 
-
 # class to define vehicle parameters
 class AckermannVehicle:
     def __init__(self,inputLength = 1,inputMaximumSteeringAngle = 1,inputMaximumVelocity = 48*pi/180):
@@ -37,12 +35,10 @@ class PPController:
     def __init__(self,inputLeadDistance,inputLength):
         self.leadDistance = inputLeadDistance
         self.length = inputLength #set the length for ppcontroller as the length of maximumSteeringAngle
+        self.turningRadius = 1
 
         # compute the steering radius of ackerman vehicle of given parameters
     def compute_turning_radius(self, inputCurrent = Point(0,0,0) , inputGoal = Point(0,0,0)):
-        global current
-        global goal
-        global alpha
 
         current = inputCurrent
         goal = inputGoal
@@ -50,19 +46,25 @@ class PPController:
         temp = ((current.heading +  3.14))
         temp = (math.fmod(temp , 2*3.14)) - 3.14
         alpha = temp - beta #angle between current heading and the line joining start-end
-        euclideanDistance = math.sqrt(math.pow((goal.x - current.x),2) + math.pow((goal.y-current.y),2))
-        turningRadius = euclideanDistance / (2*math.sin(alpha)) #this outputs the turning radius
+        L_a = math.sqrt(math.pow((goal.x - current.x),2) + math.pow((goal.y-current.y),2))
+        self.turningRadius = L_a / (2*math.sin(alpha)) #this outputs the turning radius
 
 # compute the steering angle of ackermann vehicle of given paramters
     def compute_steering_angle(self):
-        steeringAngle = 0.7*math.sin(alpha)*self.length
+
+        # Steering angle command from Pure pursuit paper:
+        # Steering angle = atan(L/R)
+        steeringAngle = math.atan(self.length / self.turningRadius)
+
+        # @senoa95: I'm commenting out this steering angle command. I did not understand this one.
+        #steeringAngle = 0.7*math.sin(alpha)*self.length
 
         return steeringAngle
 
 # compute forward velocity relative to steering angle
     def compute_forward_velocity(self): #added a variable velocity based on Bijo's suggestion
         #forwardVelocity = mule.maximumVelocity * (1 - atan(abs(steeringAngle))/(pi/2));  //this specifies the forward velocity at a given steering angle
-        forwardVelocity = 1
+        forwardVelocity = 0.4
 
         return forwardVelocity
 
