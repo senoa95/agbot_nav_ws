6a2043a875c75a4a06dcce09ab2c82949cdfcd36
senoa95 <sena@vt.edu>
2018-05-05 18:29:51 -0400

    waypoint maker is working now.

--- src/waypoint_maker/src/waypoint_maker.py
+++ src/waypoint_maker/src/waypoint_maker.py
@@ -4,32 +4,71 @@
 import rospy
 from geometry_msgs.msg import Point32,Pose,Point
 from sensor_msgs.msg import NavSatFix
+import csv
+import math
+import utm
 
 global x
+global previous_x
+global previous_y
+global current_x
+global current_y
 
-def pose_callback(pose_data):
-    rospy.loginfo(rospy.get_caller_id(), pose_data.pose_data)
-    x = pose_data.x
-    y = pose_data.y
-    x = "hahaha"
-    print(x)
+previous_x = 0
+previous_y = 0
+current_x = 0
+current_y = 0
 
 
-def attitude_callback(attitude_data):
-    rospy.loginfo(rospy.get_caller_id(), attitude_data.attitude_data)
-    yaw = attitude_data.z
+waypoint_file =  open('waypoints.txt','w')
+
+def pose_callback(data):
+
+    global x
+    global previous_x
+    global previous_y
+    global current_x
+    global current_y
+
+    # rospy.loginfo(rospy.get_caller_id(), data.data)
+    x = data.latitude
+    y = data.longitude
+    iniY,iniX, zoneNum , char = utm.from_latlon(x,y)
+    current_x = iniX
+    current_y = iniY
+
+# def heading_callback(heading_data):
+#     rospy.loginfo(rospy.get_caller_id(), heading_data.heading_data)
+#     yaw = heading_data.z
 
 def waypoint_maker():
+
+    global x
+    global previous_x
+    global previous_y
+    global current_x
+    global current_y
+
     rospy.init_node('waypoint_maker',anonymous=True)
-    rospy.Subscriber("/agBOT/Local/Pose", Pose, pose_callback)
-    rospy.Subscriber("/imu", Pose, attitude_callback)
+    rospy.Subscriber("/fix", NavSatFix, pose_callback)
+    # rospy.Subscriber("/imu", Pose, heading_callback)
+
+    while not rospy.is_shutdown():
+
+        euclideanError = math.sqrt((math.pow((current_x-previous_x),2) + math.pow((current_y-previous_y),2)))
+        print(euclideanError)
+
+        if euclideanError > 1:
+            waypoint_file.write(str(current_x)+","+str(current_y)+"\r\n")
+            # value = [current_x,current_y]
+            # waypoint_file.write(value)
+            previous_x = current_x
+            previous_y = current_y
 
 
 
-
-
-    rospy.spin()
 
 if __name__ == '__main__':
     waypoint_maker()
-    # attitude_callback()
+    waypoint_file.close()
+    # heading_callback()
